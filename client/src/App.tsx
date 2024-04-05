54c62db96212ac1981b3b048149f3ba3cbcb96a5
Danijel Jovanović <danijel.xda@gmail.com>
2024-04-05 22:17:11 +0200

    Create sample pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/login/Login";
 import Register from "./pages/auth/sign_up/SignUp";
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Create from "./pages/create/Create";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Register />} />
-
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
 export default App;
