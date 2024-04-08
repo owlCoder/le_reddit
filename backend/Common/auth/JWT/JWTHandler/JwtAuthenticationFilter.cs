aa8911a32c629fd78ffcd3bf6580b070b38c787c
Danijel Jovanović <danijel.xda@gmail.com>
2024-04-08 13:26:24 +0200

    JWT: Add docs

--- backend/Common/auth/JWT/JWTHandler/JwtAuthenticationFilter.cs
+++ backend/Common/auth/JWT/JWTHandler/JwtAuthenticationFilter.cs
@@ -8,8 +8,15 @@ using System.Threading;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 
+/// <summary>
+/// Represents an authentication filter for JWT (JSON Web Token) authorization in ASP.NET Web API.
+/// </summary>
 public class JwtAuthenticationFilter : AuthorizeAttribute
 {
+    /// <summary>
+    /// Overrides the default authorization logic to implement JWT token-based authentication.
+    /// </summary>
+    /// <param name="actionContext">The context for the action to be authorized.</param>
     public override void OnAuthorization(HttpActionContext actionContext)
     {
         if (AuthorizeRequest(actionContext))
@@ -20,11 +27,20 @@ public class JwtAuthenticationFilter : AuthorizeAttribute
         HandleUnauthorizedRequest(actionContext);
     }
 
+    /// <summary>
+    /// Handles unauthorized requests by returning an HTTP unauthorized response.
+    /// </summary>
+    /// <param name="actionContext">The context for the action that triggered the unauthorized request.</param>
     protected override void HandleUnauthorizedRequest(HttpActionContext actionContext)
     {
         actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
     }
 
+    /// <summary>
+    /// Authorizes the incoming HTTP request by validating the JWT token.
+    /// </summary>
+    /// <param name="actionContext">The context for the action to be authorized.</param>
+    /// <returns>True if the request is authorized, otherwise false.</returns>
     private bool AuthorizeRequest(HttpActionContext actionContext)
     {
         try
@@ -55,3 +71,4 @@ public class JwtAuthenticationFilter : AuthorizeAttribute
         }
     }
 }
+
