cca855b527f71bcc544ede89c6e76cc472592e77
Danijel Jovanović <danijel.xda@gmail.com>
2024-04-08 19:35:19 +0200

    Login JWT Docs

--- client/src/services/auth/login/LoginService.tsx
+++ client/src/services/auth/login/LoginService.tsx
@@ -2,7 +2,13 @@ import axios, { AxiosResponse } from "axios";
 import IToken from "../../../interfaces/auth/jwt/IToken";
 import { API_ENDPOINT } from "../../../App";
 import ILogin from "../../../interfaces/auth/login/ILogin";
+import { saveTokenToLocalstorage } from "../../jwt/JWTTokenizationService";
 
+/**
+ * Logs in a user and saves the JWT token to local storage if successful.
+ * @param credentials The user's login credentials.
+ * @returns A Promise that resolves to a boolean indicating whether the login was successful.
+ */
 const LoginService = async (credentials: ILogin): Promise<boolean> => {
   try {
     const response: AxiosResponse<IToken> = await axios.post(
@@ -11,13 +17,14 @@ const LoginService = async (credentials: ILogin): Promise<boolean> => {
     );
 
     if (response.status === 200) {
+      // Save the token to local storage if login is successful
       saveTokenToLocalstorage({ token: response.data.token });
-      return true;
+      return true; // Return true if login is successful
     } else {
-      return false;
+      return false; // Return false if login fails
     }
   } catch {
-    return false;
+    return false; // Return false if an error occurs during login
   }
 };
 
