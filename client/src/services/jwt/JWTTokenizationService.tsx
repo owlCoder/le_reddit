be15beb62306ccad59bb96724efbfa198ba9ccf2
Danijel Jovanović <danijel.xda@gmail.com>
2024-04-08 19:34:39 +0200

    JWT Docs

--- client/src/services/jwt/JWTTokenizationService.tsx
+++ client/src/services/jwt/JWTTokenizationService.tsx
@@ -1,9 +1,16 @@
 import IToken from "../../interfaces/auth/jwt/IToken";
 
+/**
+ * Saves a JWT token to the local storage.
+ * @param data The token object containing the JWT token.
+ */
 const saveTokenToLocalstorage = (data: IToken): void => {
   localStorage.setItem("token", data.token);
 };
 
+/**
+ * Removes the JWT token from the local storage.
+ */
 const removeTokenFromLocalStorage = (): void => {
   localStorage.removeItem("token");
 };
