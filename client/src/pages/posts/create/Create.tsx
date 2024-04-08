9b1510d2d4863a5ff0496ea473fac47512956851
Danijel Jovanović <danijel.xda@gmail.com>
2024-04-08 22:32:37 +0200

    Protect create page

--- client/src/pages/posts/create/Create.tsx
+++ client/src/pages/posts/create/Create.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../../../components/navbar/Navbar";
 import CreatePostForm from "../../../components/post/create/CreatePost";
+import useAuth from "../../../contexts/use_auth/UseAuth";
+import { useNavigate } from "react-router-dom";
 
 const Create: React.FC = () => {
+  const { email } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(email)
+    if (email == null || email === "") {
+      navigate("/");
+    }
+  }, [email, navigate]);
+  
   return (
     <>
       <Navbar />
