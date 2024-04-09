9913a614abff3e34934e0662d05c8237ee2448c3
Danijel Jovanović <danijel.xda@gmail.com>
2024-04-09 14:26:14 +0200

    Fix create post loading

--- client/src/pages/posts/create/Create.tsx
+++ client/src/pages/posts/create/Create.tsx
@@ -1,19 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../../components/navbar/Navbar";
 import CreatePostForm from "../../../components/post/create/CreatePost";
 import useAuth from "../../../contexts/use_auth/UseAuth";
 import { useNavigate } from "react-router-dom";
 
 const Create: React.FC = () => {
-  const { email } = useAuth();
+  const { isLoggedIn } = useAuth();
   const navigate = useNavigate();
+  const [pageLoaded, setPageLoaded] = useState<boolean>(false);
 
   useEffect(() => {
-    console.log(email)
-    if (email == null || email === "") {
-      navigate("/");
+    // Set pageLoaded to true once the page is loaded
+    setPageLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    // Check isLoggedIn only after the page is loaded
+    if (pageLoaded) {
+      if (!isLoggedIn && pageLoaded) {
+        navigate("/");
+      }
     }
-  }, [email, navigate]);
+  }, [pageLoaded, isLoggedIn, navigate]);
   
   return (
     <>
