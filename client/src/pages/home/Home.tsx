f92d8dec6ef0523d866ce2df8e3b6c908deb445b
Danijel Jovanović <danijel.xda@gmail.com>
2024-04-10 14:52:05 +0200

    Update image path & clean up errors

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
-import CreateCommentForm from "../../components/comment/create/Comment";
 
 const Home: React.FC = () => {
 
   return (
     <>
-      <Navbar />
-      <CreateCommentForm id="1" author="me" title="" hasImage={false} imageBlobUrl="dsds" comments={[]} content="1" />
-
-     
+      <Navbar />     
     </>
   );
 };
