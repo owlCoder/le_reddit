80c1b9f180a79f9d3d54fbdadd668792f8033cf4
Danijel Jovanović <danijel.xda@gmail.com>
2024-04-11 18:01:11 +0200

    create post preview

---
+++ client/src/components/post/preview/PostPreview.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import IPost from "../../../interfaces/post/view/IPost";
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+} from "@mdxeditor/editor";
+import SubscribeButton from "../../button/SubscribeButton";
+import PostHeading from "../heading/PostHeading";
+import PostStats from "../stats/PostStats";
+import GetProfilePictureByEmailService from "../../../services/users/profile/GetProfilePictureService";
+import useAuth from "../../../contexts/use_auth/UseAuth";
+import { useNavigate } from "react-router-dom";
+
+const PostPreview: React.FC<{ post: IPost }> = ({
+  post: { Id, Author, Title, Content, HasImage, ImageBlobUrl, Comments },
+}) => {
+  const [authorImage, setAuthorImage] = useState<string>("");
+  const { email } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetch = async () => {
+      // fetch profile picture
+      const picture: string = await GetProfilePictureByEmailService(Author);
+      setAuthorImage(picture);
+    };
+
+    fetch();
+  }, [Author, email]);
+  useEffect(() => {}, []);
+
+  return (
+    <>
+      <div
+        className="flex justify-between items-center mb-4"
+        onClick={() => {
+          navigate(`/post/${Id}`);
+        }}
+      >
+        <div>
+          <PostHeading
+            imageBlobUrl={authorImage}
+            author={"u/" + Author.split("@")[0]}
+          />
+        </div>
+        <div className="flex items-center">
+          <SubscribeButton
+            onClick={() => {
+              alert("Implementiraj me");
+            }}
+          />
+        </div>
+      </div>
+
+      <h1 className="font-semibold text-3xl pl-7">{Title}</h1>
+      {/* Post content */}
+      <div className="p-4">
+        <MDXEditor
+          readOnly
+          markdown={Content}
+          className="min-h-12 w-full focus:outline-none rounded-lg focus:ring-primary-500 focus:border-primary-500"
+          plugins={[
+            headingsPlugin(),
+            listsPlugin(),
+            quotePlugin(),
+            thematicBreakPlugin(),
+            markdownShortcutPlugin(),
+          ]}
+        />
+
+        {/* picture for post */}
+        {HasImage && <img src={ImageBlobUrl} />}
+
+        {/* upvote, downvore comments count */}
+        <PostStats
+          upvotesDownvotesCount={100}
+          numberOfComments={Comments.length}
+        />
+      </div>
+      <br />
+
+      <hr className="mx-4" />
+    </>
+  );
+};
+
+export default PostPreview;
