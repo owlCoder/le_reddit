6c5f24c96b605fb495aaf320991da182fe37d786
Lundjo <lukadjelic529@gmail.com>
2024-04-17 16:59:27 +0200

    Vote class

--- backend/RedditDataRepository/classes/Votes/Vote.cs
+++ backend/RedditDataRepository/classes/Votes/Vote.cs
@@ -8,6 +8,22 @@ namespace RedditDataRepository.classes.Votes
     {
         public string Id { get; set; }
 
+        public string PostId { get; set; }
 
+        public string UserId { get; set; }
+
+        public bool Voted { get; set; }
+
+        public Vote() { }
+
+        public Vote(string userId, string postId, bool voted)
+        {
+            Id = Guid.NewGuid().ToString();
+            PartitionKey = "Vote";
+            RowKey = Id;
+            UserId = userId;
+            PostId = postId;
+            Voted = voted;
+        }
     }
 }
