ee226f34497db3b1c508c569c08c2d53eb45e8ee
Lundjo <lukadjelic529@gmail.com>
2024-04-17 21:54:00 +0200

    Implemented vote count on backend

---
+++ backend/RedditDataRepository/votes/VotesCount.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using RedditDataRepository.classes.Votes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditDataRepository.votes
+{
+    public class VotesCount
+    {
+        public static async Task<List<Vote>> Execute(CloudTable table, string postId)
+        {
+            var query = new TableQuery<Vote>().Where(TableQuery.CombineFilters(
+                TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Vote"),
+                TableOperators.And,
+                TableQuery.GenerateFilterCondition("PostId", QueryComparisons.Equal, postId)));
+
+            List<Vote> votes = new List<Vote>();
+            var queryResult = await table.ExecuteQuerySegmentedAsync(query, null);
+            votes.AddRange(queryResult.Results);
+
+
+            return votes;
+        }
+    }
+}
---
+++ backend/RedditServiceWorker/Controllers/VoteController.cs
@@ -0,0 +1,58 @@
+﻿using Common.cloud.account;
+using RedditDataRepository.votes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace RedditServiceWorker.Controllers
+{
+    [RoutePrefix("api/vote")]
+    public class VoteController : ApiController
+    {
+        [HttpGet]
+        [Route("countVotes/{postId}")]
+        public async Task<IHttpActionResult> CountVotesOnPost(string postId)
+        {
+            try
+            {
+                var result = await VotesCount.Execute(AzureTableStorageCloudAccount.GetCloudTable("votes"), postId);
+
+                int karma = 0;
+                foreach(var r in result)
+                {
+                    if (r.Voted)
+                    {
+                        ++karma;
+                    }
+                    else
+                    {
+                        --karma;
+                    }
+                }
+
+                return Ok(karma);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [HttpPost]
+        [Route("upvote/{postId}/{userId}")]
+        public async Task<IHttpActionResult> UpvotePost(string postId, string userId)
+        {
+
+        }
+
+        [HttpPost]
+        [Route("downvote/{postId}/{userId}")]
+        public async Task<IHttpActionResult> DownvotePost(string postId, string userId)
+        {
+
+        }
+    }
+}
